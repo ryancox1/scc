be696bd47d93c531b370904b0a203df4f94915f9
ryancox1 <32238599+ryancox1@users.noreply.github.com>
2025-03-29 18:57:00 -0700

    Add files via upload
    Final Project files for IT 161

---
+++ it161/fp/js/main.js
@@ -0,0 +1,8 @@
+//https://tinyurl.com/dynamic-html-checker
+document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+  
+document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
+ 
+ //set current year in span with id of this-year
+ let d = new Date(); let thisYear = d.getFullYear();
+ document.getElementById("this-year").innerHTML = thisYear;
